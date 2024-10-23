81897bb842af791842e42c69aecee4889f349583
caichangqing <>
2024-10-23 17:14:42 +0800

    Resolved merge conflicts

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,9 @@ module.exports = defineConfig({
       .use('raw-loader')
       .loader('raw-loader')
       .end();
-  }
+  },
+  // 添加 publicPath，确保正确处理静态资源路径
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-vue-blog/'  // 这里的 'my-vue-blog' 是你的 GitHub 仓库名
+    : '/'
 })
